654595e51c03e05590a08062dcba86eb11e3f432
Deyvid Santana <sdsdeyvidh@hotmail.com>
2019-06-15 19:55:40 -0300

    Created domain contracts and repositories.

---
+++ QuickBuy.Domain/Contracts/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Domain.Contracts
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity FindById(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
