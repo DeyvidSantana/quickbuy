1cf8762946211e0abc635dd0ca42f2774adf8855
Deyvid Santana <sdsdeyvidh@hotmail.com>
2019-06-15 21:29:14 -0300

    Created DbContext class with DbSet properties relationed to each entity.

---
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entities;
+using QuickBuy.Domain.ValueObjects;
+
+namespace QuickBuy.Repository.Context
+{
+    public class QuickBuyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<FormPayment> FormPayment { get; set; }
+    }
+}
