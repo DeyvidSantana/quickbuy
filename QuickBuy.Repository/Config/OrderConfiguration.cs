ddff2a9afb92fe9b6f9e5896cd7b1edcf99fb193
Deyvid Santana <sdsdeyvidh@hotmail.com>
2019-06-15 22:40:22 -0300

    Mapping configuration was made in some mapping configuration classes.

--- QuickBuy.Repository/Config/OrderConfiguration.cs
+++ QuickBuy.Repository/Config/OrderConfiguration.cs
@@ -9,7 +9,43 @@ namespace QuickBuy.Repository.Config
     {
         public void Configure(EntityTypeBuilder<Order> builder)
         {
-            throw new NotImplementedException();
+            builder
+                .HasKey(o => o.Id);
+            builder
+                .Property(o => o.OrderDate)
+                .IsRequired();
+            builder
+                .Property(o => o.UserId)
+                .IsRequired();
+            builder
+                .Property(o => o.ExpectedDeliveryDate)
+                .IsRequired();
+            builder
+                .Property(o => o.CEP)
+                .IsRequired()
+                .HasMaxLength(20);
+            builder
+                .Property(o => o.State)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder
+                .Property(o => o.City)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder
+                .Property(o => o.FullAddress)
+                .IsRequired()
+                .HasMaxLength(150);
+            builder
+                .Property(o => o.NumberAddress)
+                .IsRequired();
+            builder
+                .Property(o => o.FormPaymentId)
+                .IsRequired();
+            builder
+                .Property(o => o.FormPayment)
+                .IsRequired();
+
         }
     }
 }
--- QuickBuy.Repository/Config/UserConfiguration.cs
+++ QuickBuy.Repository/Config/UserConfiguration.cs
@@ -9,7 +9,24 @@ namespace QuickBuy.Repository.Config
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            throw new NotImplementedException();
+            builder
+                .HasKey(u => u.Id);
+            builder
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder
+                .Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(500);
+            builder
+                .Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+            builder
+                .Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(30);
         }
     }
 }
