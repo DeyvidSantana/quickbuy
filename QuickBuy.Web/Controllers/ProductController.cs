944d8b8bf92f33c14f595b1cad0a2f58be48eb2a
Deyvid Santana <sdsdeyvidh@hotmail.com>
2019-06-24 17:26:39 -0300

    GetAll and Add methods was implemented in product controller.

--- QuickBuy.Web/Controllers/ProductController.cs
+++ QuickBuy.Web/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Domain.Contracts;
+using QuickBuy.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,33 @@ namespace QuickBuy.Web.Controllers
         public ProductController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_productRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Product product)
+        {
+            try
+            {
+                _productRepository.Add(product);
+                return Created("api/product", product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
